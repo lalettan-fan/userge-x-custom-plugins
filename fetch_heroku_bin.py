35cb1a9d5fea5590258147e8ab6f3bf4d8010383
lalettan-fan <73810564+lalettan-fan@users.noreply.github.com>
2021-05-23 22:00:35 +0530

    Delete fetch_heroku_bin.py

--- fetch_heroku_bin.py
+++
@@ -1,7 +0,0 @@
-from userge import userge, Message, filters
-
-@userge.on_filters(filters.channel)
-async def find_heroku_bin(message: Message):
-    text = message.text or message.caption or ""
-    if "heroku" in text.lower():
-        await message.copy(-1001127585391)
