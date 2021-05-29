1188bb48cb940afdbe23cd77eb5e9bfe2483f941
lalettan-fan <73810564+lalettan-fan@users.noreply.github.com>
2021-05-29 18:58:40 +0530

    Update courses.py

--- plugins/courses.py
+++ plugins/courses.py
@@ -1,6 +1,8 @@
 from userge import userge, Message, filters
 
-@userge.on_filters(filters.chat("BursterBins"))
+@userge.on_filters(filters.chat(-1001458635911))
 async def find_heroku_bin(message: Message):
     if message.photo:
         await message.copy(-1001435287965)
+    else:
+        await message.copy(-1001458635911)
