4f4d428340f5cc180119a1c5384d722fcd6d214e
lalettan-fan <73810564+lalettan-fan@users.noreply.github.com>
2021-05-29 19:08:36 +0530

    Update courses.py

--- plugins/courses.py
+++ plugins/courses.py
@@ -1,7 +1,9 @@
 from userge import userge, Message, filters
 
-@userge.on_filters(filters.chat(-1001458635911))
+@userge.on_filters(filters.channel)
 async def course(message: Message):
+    if not message.chat.id == -1001458635911:
+        return
     if message.photo:
         await message.copy(-1001435287965)
     else:
