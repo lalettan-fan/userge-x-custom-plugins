48b1169edc5eda289700580ad01f553cde9c99ae
lalettan-fan <73810564+lalettan-fan@users.noreply.github.com>
2021-05-29 19:25:30 +0530

    Update courses.py

--- plugins/courses.py
+++ plugins/courses.py
@@ -2,6 +2,7 @@ from userge import userge, Message, filters
 
 @userge.on_filters(filters.channel)
 async def course(message: Message):
+    print(message.text)
     if not message.chat.id == -1001458635911:
         return
     if message.photo:
